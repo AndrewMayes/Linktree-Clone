5804ebb50dbc1808c5ffae594d8b781752a06ca7
AndrewMayes <andrewmayes757@gmail.com>
2019-09-14 01:06:39 -0400

    Heroku deploy stuff again

--- server.js
+++ server.js
@@ -34,11 +34,11 @@ app.use('/users', users);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, "client", "build")))
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 }
 
 // Starts the server
