b2c59edc5523640114fd77914e2bfab90929db30
AndrewMayes <andrewmayes757@gmail.com>
2019-09-14 00:25:52 -0400

    Restructuring file structure

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -29,6 +30,16 @@ const users = require('./routes/user');
 
 // Use Routes
 app.use('/users', users);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Starts the server
 app.listen(port, () => {
