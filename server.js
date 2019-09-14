0b2d59ee8e9a874a2d7f8e27658c4d684b55e125
AndrewMayes <andrewmayes757@gmail.com>
2019-09-14 01:18:01 -0400

    Heroku deploy v4

--- server.js
+++ server.js
@@ -35,10 +35,6 @@ app.use('/users', users);
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static(__dirname + '/client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
 }
 
 // Starts the server
