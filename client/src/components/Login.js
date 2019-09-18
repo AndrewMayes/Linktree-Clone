c58a4ec8071f5131b2776c6fc4f54d52a14633b1
AndrewMayes <andrewmayes757@gmail.com>
2019-09-18 15:13:37 -0400

    Adding initial React functionality for forms

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 const Login = () => {
+  const [userInput, setUserInput] = useReducer(
+    (state, newState) => ({...state, ...newState}),
+    {
+      username: '',
+      password: ''
+    }
+  );
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log('signup submited!');
+    
+    setUserInput({username: '', password: ''});
+  }
+
+  const onChange = e => {
+    const {name, value} = e.target;
+
+    setUserInput({[name]: value});
+
+  }
+
   return (
-    <div className="buttons">
-      <form>
-        <input type="text" placeholder="Enter username or email"/>
-        <button type="submit">Login!</button>
-      </form>
+    <div className="container">
+      <div className="buttons">
+        <form onSubmit={onSubmit}>
+          <input type="text" name="username" value={userInput.username} onChange={onChange} placeholder="Username" className="user-input"/>
+          <input type="password" name="password" value={userInput.password} onChange={onChange} placeholder="Password" autoComplete="password" className="user-input"/>
+          <button type="submit" disabled={!userInput.username || !userInput.password}>Login!</button>
+        </form>
+      </div>
     </div>
   )
 }
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 const SignUp = () => {
+  const [userInput, setUserInput] = useReducer(
+    (state, newState) => ({...state, ...newState}),
+    {
+      username: '',
+      email: '',
+      password: ''
+    }
+  );
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log('signup submited!');
+
+    setUserInput({username: '', email: '', password: ''});
+  }
+
+  const onChange = e => {
+    const {name, value} = e.target;
+
+    setUserInput({[name]: value});
+
+  }
+
   return (
-    <div className="buttons">
-      <form>
-        <input type="text" placeholder="Enter username"/>
-        <input type="text" placeholder="Enter email"/>
-        <input type="text" placeholder="Enter password"/>
-        <button type="submit">Login!</button>
-      </form>
-  </div>
+    <div className="container">
+      <div className="buttons">
+        <form onSubmit={onSubmit}>
+          <input type="text" name="username" value={userInput.username} onChange={onChange} placeholder="Username" className="user-input"/>
+          <input type="text" name="email" value={userInput.email} onChange={onChange} placeholder="Email" className="user-input"/>
+          <input type="password" name="password" value={userInput.password} onChange={onChange} placeholder="Password" autoComplete="password" className="user-input"/>
+          <button type="submit" disabled={!userInput.username || !userInput.email || !userInput.password}>Sign Up!</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
