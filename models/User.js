d6b46ce615f6e65e27f45b0bbafdba34957a8fc4
AndrewMayes <andrewmayes757@gmail.com>
2019-09-18 19:02:07 -0400

    Adding password and email to the User model and adding login/signup routes

--- models/User.js
+++ models/User.js
@@ -21,6 +21,15 @@ const UserSchema = new Schema({
     unique: true,
     trim: true
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
   date: {
     type: Date,
     default: Date.now
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 
 // User Model
 const User = require('../models/User');
@@ -21,19 +25,6 @@ router.get('/:username', (req, res) => {
   const username = req.params.username
   const queryUsername = '^' + username + '$'
   User.findOne({ "username": { '$regex': queryUsername, $options: 'i' } })
-    .then(user => res.json(user))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
-// @route POST /users
-// @desc Create a user
-// @access Public
-router.post('/', (req, res) => {
-  const newUser = new User({
-    username: req.body.username
-  });
-
-  newUser.save()
     .then(user => res.json(user))
     .catch(err => res.status(400).json('Error: ' + err));
 });
@@ -65,6 +56,110 @@ router.delete('/:username', (req, res) => {
     .then(() => res.json({success: true}))
     .catch(err => res.status(400).json('Error: ' + err));
 });
+
+
+
+// Auth routes
+
+const jwtSecret = process.env.jwtSecret;
+
+// @route POST /users
+// @desc Create a user (Register)
+// @access Public
+router.post('/', (req, res) => {
+  const { username, email, password } = req.body;
+
+  // Validation
+  if (!username || !email || !password) {
+    return res.status(400).json({msg: 'Please enter all fields'})
+  }
+
+  // Check for existing user
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({msg: 'User already exists'})
+      }
+
+      const newUser = new User({
+        username,
+        email,
+        password
+      });
+
+      // Create salt & hash
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save()
+            .then(user => {
+              jwt.sign(
+                { id: user.id },
+                jwtSecret,
+                { expiresIn: 3600 },
+                (err, token) => {
+                  if (err) throw err;
+                  res.json({
+                    token,
+                    user: {
+                      id: user.id,
+                      username: user.username,
+                      email: user.email
+                    }
+                  });
+                }
+              )
+            });
+        })
+      })
+    })
+});
+
+// @route POST /users/auth
+// @desc Auth user (Login)
+// @access Public
+router.post('/auth', (req, res) => {
+  const { username, password } = req.body;
+
+  // Validation
+  if (!username || !password) {
+    return res.status(400).json({msg: 'Please enter all fields'})
+  }
+
+  // Check for existing user
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        return res.status(400).json({msg: 'User does not exist'})
+      }
+
+      // Validate Password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+
+          jwt.sign(
+            { id: user.id },
+            jwtSecret,
+            { expiresIn: 3600 },
+            (err, token) => {
+              if (err) throw err;
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  username: user.username,
+                  email: user.email
+                }
+              });
+            }
+          )
+        })
+      })
+});
+
+
 
 
 
