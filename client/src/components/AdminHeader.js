5400417afce4e49dadb4936b1d66f27d056a1277
AndrewMayes <andrewmayes757@gmail.com>
2019-09-26 17:36:26 -0400

    Adding user logout functionality

--- client/src/components/AdminHeader.js
+++ client/src/components/AdminHeader.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const AdminHeader = ({ admin, settings }) => {
+
+  const logOut = () => {
+    localStorage.removeItem('auth-token');
+    window.location = '/';
+  }
+
   return (
     <div className="admin-header">
       <Link to='/admin' className={`header-links ${admin}`}>Links</Link>
       <Link to='/settings' className={`header-links ${settings}`}>Settings</Link>
+      <h1 className='header-links' onClick={logOut}>Log Out</h1>
     </div>
   )
 }
