9009fd5a44f23631fedce80a2f9c124c97934779
AndrewMayes <andrewmayes757@gmail.com>
2019-09-28 17:07:52 -0400

    Adding mobile responsive design for admin links section

--- client/src/components/NewLink.js
+++ client/src/components/NewLink.js
@@ -40,13 +40,13 @@ const NewLink = ({ username }) => {
   }
 
   return (
-    <div className="newlink-buttons">
-      <form onSubmit={onSubmit}>
-      <input type="text" name="linkTitle" value={linkInput.linkTitle} onChange={onChange} placeholder="Title" className="user-input"/>
-      <input type="text" name="url" value={linkInput.url} onChange={onChange} placeholder="URL" className="user-input"/>
-      <button type="submit" disabled={!linkInput.linkTitle || !linkInput.url} className="user-submit">Submit</button>
-      </form>
-    </div>
+    <form onSubmit={onSubmit}>
+      <div className="newlink-buttons">
+        <input type="text" name="linkTitle" value={linkInput.linkTitle} onChange={onChange} placeholder="Title" className="user-input"/>
+        <input type="text" name="url" value={linkInput.url} onChange={onChange} placeholder="URL" className="user-input"/>
+        <button type="submit" disabled={!linkInput.linkTitle || !linkInput.url} className="user-submit">Submit</button>
+      </div>
+    </form>
   )
 }
 
