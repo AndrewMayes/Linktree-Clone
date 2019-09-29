efc0107d6e14164207f02b1db39b604eaa29eb87
AndrewMayes <andrewmayes757@gmail.com>
2019-09-28 23:26:44 -0400

    Beginning the Settings component

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import AdminHeader from './AdminHeader';
+import avatar from '../imgs/default.png';
+import AvatarComponent from './AvatarComponent'
 
 const Settings = () => {
+
+  const onClick = () => {
+    console.log('clickeee');
+  }
+
   return (
     <>
       <AdminHeader settings={'active'}/>
-      <div>
-        this is the settings page
+      <div className="settings-container">
+        <div className="avatar-container">
+          <div className="avatar">
+            <input type="image" src={avatar} name="change-avatar" className="change-avatar-style" onClick={onClick}/>
+            {/*<AvatarComponent />*/}
+          </div>
+          <div className="avatar-form">
+            <div className="remove-button" onClick={onClick}>Remove</div>
+          </div>
+        </div>
+        <div className="color-div">
+          temp
+        </div>
       </div>
     </>
   )
