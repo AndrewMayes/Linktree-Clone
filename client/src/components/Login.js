725f46710a1316746298b3db89dbf7ba4235b5d6
AndrewMayes <andrewmayes757@gmail.com>
2019-10-01 18:27:39 -0400

    Adding validation UI to login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import formValidation from './FormValidation';
 import inputErrors from './InputErrors';
@@ -10,7 +10,12 @@ const Login = () => {
     password: ''
   };
 
+  const [userNotFound, setUserNotFound] = useState(false);
+  const [wrongPassword, setWrongPassword] = useState(false);
+
   const axiosFunc = () => {
+    setUserNotFound(false);
+    setWrongPassword(false);
     axios.post(`/users/auth`, values )
       .then(res => {
         // Save JWT token in localStorage
@@ -20,24 +25,34 @@ const Login = () => {
         window.location = '/admin';
       })
       .catch(err => {
-        console.log(err);
-        alert('Credentials do not match our records');
+        const error = err.response.data
+        if (error === 'Username/Email not found') {
+          setUserNotFound(true);
+        }
+        if (error === 'Invalid password') {
+          setWrongPassword(true);
+        }
       })
   }
 
   const {handleSubmit, handleChange, values, errors, isSubmitting} = formValidation(initialState, inputErrors, axiosFunc);
 
-
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="login-buttons">
-        <input type="text" name="username" value={values.username} onChange={handleChange} placeholder="Username" className="user-input"/>
-        {errors.username && <p className="error-text">{errors.username}</p>}
-        <input type="password" name="password" value={values.password} onChange={handleChange} placeholder="Password" autoComplete="password" className="user-input"/>
-        {errors.password && <p className="error-text">{errors.password}</p>}
-        <button type="submit" disabled={isSubmitting} className="user-submit">Login!</button>
+    <>
+      <div className="error-msg">
+        <p>{userNotFound ? 'Username/Email not found' : ''}</p>
+        <p>{wrongPassword ? 'Invalid Password' : ''}</p>
+        <p>{errors.username ? 'Required Username/Email.' : ''}</p>
+        <p>{errors.password ? errors.password : ''}</p>
       </div>
-    </form>
+      <form onSubmit={handleSubmit}>
+        <div className="login-buttons">
+          <input type="text" name="username" value={values.username} onChange={handleChange} placeholder={'Username or Email'} className={errors.username ? 'user-input error-text' : 'user-input'}/>
+          <input type="password" name="password" value={values.password} onChange={handleChange} placeholder={'Password'} autoComplete="password" className={errors.password ? 'user-input error-text' : 'user-input'}/>
+          <button type="submit" disabled={isSubmitting} className="user-submit">Login!</button>
+        </div>
+      </form>
+    </>
   )
 }
 
