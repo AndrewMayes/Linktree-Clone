e51262b6c10005370bb7adf0d2af6b611b218dfc
AndrewMayes <andrewmayes757@gmail.com>
2019-10-04 18:44:15 -0400

    Fixing issue where user-inputed links without URL protocol would not load

--- client/src/components/Link.js
+++ client/src/components/Link.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Link = ({ link }) => {
+  const formatURL = link => {
+    const substring = "//";
+    if (link.url.includes(substring)) {
+      return link.url;
+    } else {
+      const newURL = `//${link.url}`;
+      return newURL;
+    }
+  }
+
   return (
     <div className="link">
-      <a className="linkContainer" href={link.url} target="_blank" rel="noopener noreferrer">{link.linkTitle}</a>
+      <a className="linkContainer" href={formatURL(link)} target="_blank" rel="noopener noreferrer">{link.linkTitle}</a>
     </div>
   )
 }
