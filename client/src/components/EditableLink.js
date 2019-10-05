cc1f3011c3468df44f74a5fa8fb9e4c7ada2f774
AndrewMayes <andrewmayes757@gmail.com>
2019-10-05 14:01:41 -0400

    Users can now edit urls of their links

--- client/src/components/EditableLink.js
+++ client/src/components/EditableLink.js
@@ -20,11 +20,17 @@ const EditableLink = ({ id, username, link, rerender }) => {
   }
 
   const [editTitle, setEditTitle] = useState(false);
+  const [editURL, setEditURL] = useState(false);
 
   const axiosFunc = () => {
     axios.patch(`/users/${username}/editlink`, values, config)
       .then(res => {
-        toggleEditTitle();
+        if (editTitle === true) {
+          toggleEditTitle();
+        }
+        if (editURL === true) {
+          toggleEditURL();
+        }
       })
       .catch(err => {
         console.log(err);
@@ -47,27 +53,42 @@ const EditableLink = ({ id, username, link, rerender }) => {
     setEditTitle(!editTitle);
   }
 
+  const toggleEditURL = () => {
+    setEditURL(!editURL);
+  }
+
   return (
     <div className="edit-link-container">
       <span className="delete-button" onClick={deleteLink}>
         <ReactSVG src={deleteSVG} />
       </span>
       <div className="edit-linkTitle">
-        <div className="edit-linkTitle-title">
-        {(editTitle) ? 
-          <form onSubmit={handleSubmit}>
-            <div>
-              <input type="text" name="linkTitle" value={values.linkTitle} onChange={handleChange} autoFocus="autofocus" onBlur={handleSubmit} placeholder={'Title'} className={errors.linkTitle ? 'edit-link-input error-text' : 'edit-link-input'}/>
-            </div>
-          </form> 
-          : values.linkTitle}
+        <div className="edit-linkTitle-wrap">
+          {(editTitle) ? 
+            <form onSubmit={handleSubmit}>
+              <div>
+                <input type="text" name="linkTitle" value={values.linkTitle} onChange={handleChange} autoFocus="autofocus" onBlur={handleSubmit} placeholder={'Title'} className={errors.linkTitle ? 'error-url-text' : 'edit-link-input'}/>
+              </div>
+            </form> 
+            : values.linkTitle}
         </div>
         <span className="edit-button" onClick={toggleEditTitle}>
           {(editTitle) ? '' : <ReactSVG src={editSVG} />}
         </span>
       </div>
       <div className="edit-url">
-        {link.url}
+        <div className="edit-url-wrap">
+          {(editURL) ? 
+            <form onSubmit={handleSubmit}>
+              <div>
+                <input type="text" name="url" value={values.url} onChange={handleChange} autoFocus="autofocus" onBlur={handleSubmit} placeholder={'URL'} className={errors.url ? 'error-url-text' : 'edit-link-input'}/>
+              </div>
+            </form> 
+          : values.url}
+        </div>
+        <span className="edit-button" onClick={toggleEditURL}>
+          {(editURL) ? '' : <ReactSVG src={editSVG} />}
+        </span>
       </div>
     </div>
   )
